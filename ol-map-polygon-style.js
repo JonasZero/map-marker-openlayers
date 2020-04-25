97b82aace6168938f06595fb1ae9476ff31ac142
Moovspace <fonekpapa@gmail.com>
2020-04-25 22:40:32 +0200

    Openlayer map polygon colors
    Change map polygon colors.

---
+++ ol-map-polygon-style.js
@@ -0,0 +1,18 @@
+/*
+    Polygon color change
+*/
+var FillColor = 'rgba(71, 166, 255, 0.2)';
+var LineColor = 'rgba(71, 166, 255, 1)';
+
+var style = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: FillColor,
+        weight: 1
+    }),
+    stroke: new ol.style.Stroke({
+        color: LineColor,
+        width: 2,
+        lineDash: [5,10]
+    })
+});
+vectorLayer.setStyle(style);
